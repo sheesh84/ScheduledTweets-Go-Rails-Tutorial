524b4351023c818465b0c46b8282e270c81d89e1
Sheesh84 <wjschichtl@gmail.com>
2021-08-13 10:58:52 -0500

    Updating from tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,24 @@ Rails.application.routes.draw do
   # GET root
   root to: 'main#index', as: :root
 
-  # GET /about
+  # GET /sign_up
+  get "sign_up", to: "registrations#new", as: :registrations
+  # POST /sign_up
+  post "sign_up", to: "registrations#create"
+
+  # GET /sign_in
+  get "sign_in", to: "sessions#new"
+  # POST /sign_in
+  post "sign_in", to: "sessions#create"
+
+
+  # DELETE /logout
+  delete "logout", to: "sessions#destroy"
+
+  # GET /about-us
   get 'about-us', to: 'about#index', as: :about
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
